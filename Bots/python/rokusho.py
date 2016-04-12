1c72aa6a279801314223ff9e9f57564b215bee59
Fabio Diniz <fabiomachadodiniz@gmail.com>
2016-04-11 21:36:49 -0300

    roskusho first import

---
+++ Bots/python/rokusho.py
@@ -0,0 +1,16 @@
+from bot_interface import *
+import math
+
+
+
+class Rokusho(BotBase):
+
+    def process(self, gamestate):
+
+        return Action.SHOOT
+
+
+
+
+
+GameState(Rokusho()).connect()
